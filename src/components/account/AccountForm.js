6e9f717751fd48eb7b18da702d4b0888122d3da6
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2024-04-02 00:19:38 -0300

    gravando no banco

--- src/components/account/AccountForm.js
+++ src/components/account/AccountForm.js
@@ -1,14 +1,29 @@
+import { useState } from 'react';
 import Input from '../form/Input';
 import SubmitButton from '../form/SubmitButton';
 import styles from './AccountForm.module.css'
 
-function AccountForm({btnText}) {
+function AccountForm({handleSubmit, btnText, contasData}) {
+    const [conta, setConta] = useState(contasData || {})
+
+    const submit = (e) => {
+        e.preventDefault()
+        // console.log('conta', conta)
+        handleSubmit(conta)
+    }
+
+    function handleChange(e){
+        setConta({ ...conta, [e.target.name]: e.target.value})
+        //console.log('conta', conta)
+    }
+
   return (
-    <form className={styles.form}>
-      <Input type="text" text="Titulo" name="name" placeholder="Insira o titulo da conta"/>
-      <Input type="number" text="Valor" name="valor" placeholder="Insira o valor da Conta"/>
-      <Input type="date" text="Data de vencimento" name="data_venc" placeholder="Insira data de vencimento da Conta"/>
-      <Input type="number" text="Taxa de juros" name="juros" placeholder="Insira a taxa de juros da Conta"/>    
+    <form onSubmit={submit} className={styles.form}>
+      <Input type="text" text="Titulo" name="name" placeholder="Insira o titulo da conta" handleOnChange={handleChange} value={conta.name ? conta.name : ''}/>
+      <Input type="number" text="Valor" name="valor" placeholder="Insira o valor da Conta" handleOnChange={handleChange} value={conta.valor ? conta.valor : ''}/>
+      <Input type="date" text="Data de vencimento" name="data_venc" placeholder="Insira data de vencimento da Conta" handleOnChange={handleChange} value={conta.data_venc ? conta.data_venc : ''}/>
+      <Input type="number" text="Taxa de juros" name="juros" placeholder="Insira a taxa de juros da Conta" handleOnChange={handleChange} value={conta.juros ? conta.juros : ''}/>
+      
       <SubmitButton text={btnText}   />
     </form>
   );
--- src/components/pages/NovaConta.js
+++ src/components/pages/NovaConta.js
@@ -1,12 +1,37 @@
+import { useNavigate } from 'react-router-dom'
+
 import styles from './NovaConta.module.css'
 
 import AccountForm from '../account/AccountForm'
 function NovaConta(){
+
+    const navigate = useNavigate()
+
+    function createPost(conta){
+
+
+        fetch("http://localhost:5000/contas",{
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSON.stringify(conta)
+        })
+        .then(resp => resp.json())
+        .then((data) => {
+            console.log(data)
+            //redirect
+            //history.push('/contas', { message: 'Conta cadstrada com sucesso'})
+            navigate("/contas", { message: "PConta cadstrada com sucesso!" })
+        })
+        .catch(err => console.log(err))
+
+    }
     return (
         <div className={styles.new_account_container}>
             <h1>Cadastrar Conta</h1>
             <p>Cadastre a conta para organizar a sua saúde financeira</p>
-            <AccountForm btnText="Cadastrar Conta"/>
+            <AccountForm handleSubmit={createPost} btnText="Cadastrar Conta"/>
         </div>
     )
 }
