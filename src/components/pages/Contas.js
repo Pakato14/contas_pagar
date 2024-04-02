7426dee40a612ff1039f6017aeaeda17a263241f
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2024-04-02 19:52:59 -0300

    organizar o dashboard

--- src/components/pages/Contas.js
+++ src/components/pages/Contas.js
@@ -1,17 +1,35 @@
 import { useLocation } from "react-router-dom";
+import { useEffect, useState } from 'react';
 
 import Message from "../layout/Message";
 import styles from "./Contas.module.css";
 import Container from '../layout/Container';
 import LinkButton from "../layout/LinkButton";
+import AccountCard from "../account/AccountCard"
 
 //isso é uma página project do tutorial
 function Contas() {
+    const [contas, setContas] = useState([])
   const location = useLocation();
   let message = "";
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch('http://localhost:5000/contas',{
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(resp => resp.json())
+    .then((data) => {
+      console.log('data', data)
+      setContas(data)
+    })
+    .catch((err) => console.log(err))
+  }, [])
   return (
     <div className={styles.account_details}>
       <div className={styles.details_container}>
