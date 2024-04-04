06b3adc75b82ac064479bfedfa6f7906ef047e54
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2024-04-03 23:36:21 -0300

    loading

--- src/components/pages/Contas.js
+++ src/components/pages/Contas.js
@@ -5,11 +5,13 @@ import Message from "../layout/Message";
 import styles from "./Contas.module.css";
 import Container from '../layout/Container';
 import LinkButton from "../layout/LinkButton";
+import Loading from "../layout/Loading";
 import AccountCard from "../account/AccountCard"
 
 //isso é uma página project do tutorial
 function Contas() {
     const [contas, setContas] = useState([])
+    const [removeLoading, setRemoveLoading]  = useState(false)
   const location = useLocation();
   let message = "";
   if (location.state) {
@@ -27,9 +29,14 @@ function Contas() {
     .then((data) => {
       console.log('data', data)
       setContas(data)
+      setRemoveLoading(true)
     })
     .catch((err) => console.log(err))
   }, [])
+
+  function removeConta(id){
+    
+  }
   return (
     <div className={styles.account_details}>
       <div className={styles.details_container}>
@@ -48,7 +55,10 @@ function Contas() {
             key={conta.id}
             />
         ))}
-        
+        {!removeLoading && <Loading />}   
+        {removeLoading && contas.length === 0 &&(
+          <p>Não há projetos cadastrados!</p>
+        )}     
       </Container>
     </div>
   );
