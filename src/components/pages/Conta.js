fd766785c063494e33f06b1e05a7157cd7bb6790
Daniel Araujo <daniel.araujo@sedet.ce.gov.br>
2024-04-04 17:16:32 -0300

    editando conta

--- src/components/pages/Conta.js
+++ src/components/pages/Conta.js
@@ -4,11 +4,15 @@ import { useParams } from 'react-router-dom'
 import { useEffect, useState } from 'react';
 import Loading from '../layout/Loading';
 import Container from '../layout/Container';
+import AccountForm from '../account/AccountForm';
+import Message from '../layout/Message';
 
 function Conta(){
     const { id } = useParams()
     const [account, setAccount] = useState([])
     const [showProjectForm, setShowProjectForm] = useState(false)
+    const [message, setMessage] = useState()
+    const [type, setType] = useState()
 
     useEffect(() => {
         setTimeout(() =>{
@@ -28,6 +32,29 @@ function Conta(){
     }, [id])
     console.log(id)
 
+    function editPost(account){
+        // if(compara a data de vencimento){
+        //     setMessage(`O Boleto está com dias atrasado`)
+        //     setType('error')
+        //     return false
+        // }
+        fetch(`http://localhost:5000/contas/${account.id}`, {
+            method: 'PATCH',
+            headers:{
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(account)
+        })
+        .then((resp) => resp.json())
+        .then((data) => {
+            setAccount(data)
+            setShowProjectForm(false)
+            setMessage('Dados da conta atualizado!')
+            setType('success')
+        })
+        .catch((err) => console.log(err))
+    }
+
     function toggleProjectForm(){
         setShowProjectForm(!showProjectForm)
     }
@@ -36,6 +63,7 @@ function Conta(){
         {account.name? (
             <div className={styles.account_details}>
                 <Container customClass="column">
+                    {message && <Message type={type} msg={message} />}
                 <div className={styles.details_container}>
                     <h1>Conta: {account.name}</h1>
                     <button className={styles.btn} onClick={toggleProjectForm}>{!showProjectForm ? 'Editar conta' : 'Fechar'}</button>
@@ -56,7 +84,7 @@ function Conta(){
                         </div>
                     ): (
                         <div className={styles.form}>
-                            <p>detalhes do projeto</p>
+                            <AccountForm handleSubmit={editPost} btnText="Concluir edição" contasData={account} />
                         </div>
                     )}
                 </div>
